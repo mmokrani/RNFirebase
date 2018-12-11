bc6300049c5f109ac2d29fab831782c73880d071
Mehdi <admin@MacBook-Air-de-Mehdi.local>
2018-12-11 17:00:02 +0100

    configuration pour la Release

---
+++ VeryExpensive.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class OtherComponent extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Test : OtherComponent</Text>
+      </View>
+    );
+  }
+}
+
+export default OtherComponent;
--- ios/RNFirebase/AppDelegate.m
+++ ios/RNFirebase/AppDelegate.m
@@ -20,7 +20,12 @@
   [FIRApp configure];
   [RNFirebaseNotifications configure];
   NSURL *jsCodeLocation;
+
+#ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"RNFirebase"
@@ -33,6 +38,9 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  UIView* launchScreenView = [[[NSBundle mainBundle] loadNibNamed:@"LaunchScreen" owner:self options:nil] objectAtIndex:0];
+  launchScreenView.frame = self.window.bounds;
+  rootView.loadingView = launchScreenView;
   return YES;
 }
 
